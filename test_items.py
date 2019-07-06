c1ce9240720ac9e4d5b9add137cdbb469d57828e
bochanikc <nikitos564@gmail.com>
2019-07-06 21:34:58 +0300

    решение задания на шаге 9 урок 3.6

---
+++ test_items.py
@@ -0,0 +1,7 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    button = browser.find_element_by_class_name("btn-add-to-basket")
+    value = button.text
+    print("\n" + value)
